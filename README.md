df75c34815cd6358b0030a6ce6d41483757f1d62
Amartya Mondal <atm1504.in@gmail.com>
2020-06-07 04:07:50 +0530

    feat: Final setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mongoflask
 A flask app to learn using mongodb in python
+
+## Steps to run
+* Create a virtual environment and activate it
+* Execute ```pip3 install -r requirements.txt```
+* Add the mongo db url in the ```config.py``` file
+* Now run the app by executing ```python3 app.py```
--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/amartyamondal/Documents/project/mongoFlask/mongodb_flask_env"
+set -gx VIRTUAL_ENV "/Users/amartyamondal/Documents/project/mongoFlask/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(mongodb_flask_env) "
-            printf "%s%s" "(mongodb_flask_env) " (set_color normal)
+        if test -n "(env) "
+            printf "%s%s" "(env) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
