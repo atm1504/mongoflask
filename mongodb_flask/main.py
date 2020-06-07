1664175ece3959eeb62433e10927f461969db63c
Amartya Mondal <atm1504.in@gmail.com>
2020-06-07 19:50:18 +0530

    feat: Normal mongo operations

--- mongodb_flask/main.py
+++ mongodb_flask/main.py
@@ -7,5 +7,19 @@ main = Blueprint("main", __name__)
 def index():
     user_collection = mongo.db.users
     # user_collection.insert({"name": "Atreyee"})
-    user_collection.insert_one({"name": "Atreyee"})
+    user_collection.insert({"name": "Atreyee Mukherjee"})
     return "<h1>Added a name</h1>"
+
+@main.route('/find')
+def find():
+    user_collection = mongo.db.users
+    user = user_collection.find_one({"name": "Atreyee"})
+    return f'<h1>User: {user["name"]} id: {user["_id"]} </h1>'
+
+@main.route('/update')
+def update():
+    user_collection = mongo.db.users
+    user = user_collection.find_one({"name": "Atreyee"})
+    user["gender"] = "Female"
+    user_collection.save(user)
+    return "<h1>Updated</h1>"
